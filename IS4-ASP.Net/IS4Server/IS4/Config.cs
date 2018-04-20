37a54a46346fed4fce0179747cd7bfbd59e2e1ef
Scott Alexander <scott.alexander@civica.co.uk>
2018-04-20 12:02:09 +0100

    Default Client Added - Working

--- IS4-ASP.Net/IS4Server/IS4/Config.cs
+++ IS4-ASP.Net/IS4Server/IS4/Config.cs
@@ -74,8 +74,8 @@ namespace IS4Server.IS4
                         new Secret("secret".Sha256())
                     },
 
-                    RedirectUris = { "http://localhost:5002/signin-oidc" },
-                    PostLogoutRedirectUris = { "http://localhost:5002/signout-callback-oidc" },
+                    RedirectUris = { "https://localhost:44362/signin-oidc" },
+                    PostLogoutRedirectUris = { "https://localhost:44362/signout-callback-oidc" },
 
                     AllowedScopes =
                     {
